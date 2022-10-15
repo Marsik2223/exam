a9e38a4784e96a31fd15651b8e90a82de1c7518d
Marsik <mara_ru88@mail.ru>
2022-10-15 22:24:55 +0300

    fsdf

--- exam_003/Program.cs
+++ exam_003/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 30); // 1 2 3 .. 30
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 30); // 1 2 3 .. 30
+Console.WriteLine(numberB);
 Console.WriteLine (numberA + numberB);
