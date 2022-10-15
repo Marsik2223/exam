0ad9b0a0e861c62f3d94cfc3a9825087a956f076
Marsik <mara_ru88@mail.ru>
2022-10-15 22:43:51 +0300

    dgds

---
+++ exam_005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите свое имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "рома")
+{
+    Console.WriteLine("Я тебя люблю!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
